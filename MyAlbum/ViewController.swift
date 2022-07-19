adf74e4e60228cb34525a1ee4642d60ba48d8806
parksangau <devsangau@gmail.com>
2022-07-20 03:44:13 +0900

    fast campus iOS

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func challenge(_ sender: UIButton) {
+        let alert = UIAlertController(title: "도오오오전입니다", message: "도오오전 과제는 너무 행복해!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
