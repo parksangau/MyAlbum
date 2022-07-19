60367c4895c5dc51b2f59af7994a734f0a959009
parksangau <devsangau@gmail.com>
2022-07-20 04:15:24 +0900

    String Variable 이해하기

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -8,17 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showAlert(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello", message: "My First App!", preferredStyle: .alert)
+        let message = "가격은 ￦\(currentValue) 입니다."
+        
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
     }
     
 }
